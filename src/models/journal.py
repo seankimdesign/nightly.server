e0396dbf2796ff3afb72b2195922bb91eec4423c
Sean Kim <sean@seankimdesign.com>
2018-01-20 13:47:39 -0800

    Initial /list implementation

--- src/models/journal.py
+++ src/models/journal.py
@@ -4,13 +4,13 @@ from db import db
 class JournalModel(db.Model):
     __tablename__ = 'journals'
 
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    username = db.Column(db.Integer, db.ForeignKey('users.username'), primary_key=True)
     date = db.Column(db.String(10), primary_key=True)
     content = db.Column(db.Text)
     private = db.Column(db.Boolean)
 
-    def __init__(self, user_id, date, content, private=False):
-        self.user_id = user_id
+    def __init__(self, username, date, content, private=False):
+        self.username = username
         self.date = date
         self.content = content
         self.private = private
@@ -21,13 +21,19 @@ class JournalModel(db.Model):
 
     def to_dict(self):
         return {
-            "user_id": self.user_id,
+            "username": self.username,
             "date": self.date,
             "content": self.content,
             "private": self.private
         }
 
     @classmethod
-    def retrieve_by_user_id(cls, user_id):
-        if user_id:
-            return cls.query.filter_by(user_id=user_id).all()
+    def retrieve_by_username(cls, username, count=None, daterange=10):
+        if username:
+            print(count)
+            if count == 0:
+                return cls.query.filter_by(username=username).all()
+            elif count:
+                return cls.query.filter_by(username=username).limit(count).all()
+            else:
+                pass
--- src/resources/list.py
+++ src/resources/list.py
@@ -6,8 +6,34 @@ from models.journal import JournalModel
 
 class List(Resource):
 
-    def get(self, username=None):
-        print(request.args)
-        if username:
-            return {"message": "Username given: {}".format(username)}
-        return {"message": "No username given"}
+    def get(self, username):
+        n = request.args.get('n')
+        d = request.args.get('d')
+        try:
+            n = int(n) if n else None
+            d = int(d) if d else None
+        except ValueError:
+            return {"message": "Invalid request"}, 400
+        entries = JournalModel.retrieve_by_username(username, count=n, daterange=d)
+        print(entries)
+        return List.format_response(entries, username, count=n, daterange=d)
+
+
+    @classmethod
+    def format_response(cls, entries, target, count=None, daterange=None):
+        if entries and len(entries) > 0:
+            entries_obj = {
+                "entries": [entry.to_dict() for entry in entries]
+            }
+            query_obj = {
+                "target": target,
+                "matching": len(entries)
+            }
+            if count is not None:
+                query_obj['count'] = count
+            elif daterange:
+                query_obj['range'] = daterange
+            return {
+                "query": query_obj,
+                "entries": entries_obj
+            }
